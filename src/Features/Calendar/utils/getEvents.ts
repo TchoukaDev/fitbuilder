8cf9596feb068fbcb3a0209412e3298540ca88c5
Romain Wirth <romain.wirth@gmail.com>
2026-02-03 11:35:26 +0100

    fix getEvents.ts

--- src/Features/Calendar/utils/getEvents.ts
+++ src/Features/Calendar/utils/getEvents.ts
@@ -4,7 +4,7 @@ import connectDB from "@/libs/mongodb";
 import { ObjectId } from "mongodb";
 import { getColorByStatus } from "./getColorByStatus";
 import { CalendarEvent } from "@/types/calendarEvent";
-import { WorkoutSession } from "@/types/workoutSession";
+import { WorkoutSessionDB } from "@/types/workoutSession";
 
 export async function getEvents(userId: string): Promise<CalendarEvent[]> {
   const db = await connectDB();
@@ -15,22 +15,22 @@ export async function getEvents(userId: string): Promise<CalendarEvent[]> {
   if (!user) {
     throw new Error("Utilisateur non trouvé");
   }
-  const sessions = user?.sessions || [];
+  const sessions = user?.sessions || []
 
   // Transformer en événements calendrier
-  return sessions.map((session: WorkoutSession) => {
+  return sessions.map((session: WorkoutSessionDB) => {
     const start = new Date(session.scheduledDate);
     const durationMs = (session.estimatedDuration || 60) * 60 * 1000;
     const end = new Date(start.getTime() + durationMs);
 
     return {
-      id: session.id,
+      id: session._id.toString(),
       title: session.workoutName,
       start: start,
       end: end,
       resource: {
         ...session,
-        id: session.id,
+        id: session._id.toString(),
         userId: session.userId,
         workoutId: session.workoutId,
       },
